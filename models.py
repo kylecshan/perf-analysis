df0b6311aa142a07ef029a52c37360fe5f3acbbe
Kyle Shan <kylecshan@gmail.com>
2020-01-28 23:51:02 -0800

    change model to generalized likelihood ratio

--- models.py
+++ models.py
@@ -1,32 +1,41 @@
 import numpy as np
+import pandas as pd
 from utils import *
+    
+def weightedStd(priorStd, priorN, x):
+    """Weighted average of a prior estimate of std and std of current sample
+    """
+    n = len(x)
+    if n < 4:
+        return priorStd
+    
+    std = trimStd(x)
+    postStd = (priorN * priorStd + n * std) / (priorN + n)
+    return postStd
 
-def batchCusum(x, mean, std, min_shift = .5, max_influence = 4):
+def cusum(x, mean, std, threshold):
     """CUSUM algorithm for change point detection
     x  : 1D np.array
     mean: estimated center of the distribution of x[0]
     std: estimated standard deviation of the distribution of x[0]
-    min_shift: minimum shift size to consider
-    max_influence: the maximum influence of a single data point
+    threshold: minimum likelihood ratio for detection
+    
+    returns: index of first detected change point
     """
     
     # Normalize x
     x = (x - mean) / std
     
-    # Compute positive and negative CUSUM stats
-    Cp = np.zeros_like(x)
-    Cn = np.zeros_like(x)
-    for i in range(len(x)):
-        if i > 0:
-            psignal = np.min([max_influence, x[i] - min_shift])
-            nsignal = np.min([max_influence, -x[i] - min_shift])
-            Cp[i] = np.max([0, psignal + Cp[i-1]])
-            Cn[i] = np.max([0, nsignal + Cn[i-1]])
-            
-    # Final statistic is max of Cp and Cn
-    return np.maximum(Cp, Cn)
-
-def cusum(x, startup = 10, threshold=6, max_influence=4, verbose=False):
+    # Find minimum n that yields a detection
+    lrs = np.array([np.nan])
+    for n in range(2, len(x)+1):
+        S = np.cumsum(x[:n])
+        lrs = np.array([np.abs(S[n-1]-S[k])/np.sqrt(n-k-1) for k in range(n-1)])
+        if np.any(lrs >= threshold):
+            break
+    return lrs
+    
+def findChangePts(x, startup = 10, threshold=6, verbose=False):
     """Apply CUSUM algorithm after each new data point
     x: 1D np.array with at least (startup) points
     startup: number of points to bootstrap initial std guess
@@ -50,19 +59,15 @@ def cusum(x, startup = 10, threshold=6, max_influence=4, verbose=False):
     j = i+1
     while j < n:
         # Estimate variance of current regime
-        stdEsts[j] = weightedStd(2*preStd, preWt, x[i:j])
+        stdEsts[j] = weightedStd(preStd, preWt, x[i:j])
         
         # Calculate CUSUM stat and check if it's over the threshold
-        batchOutput = batchCusum(x[i:j], np.median(x[i:j]), stdEsts[j], max_influence=max_influence)
-        cusumStats[-1][j] = batchOutput[-1]
-        if verbose: 
-            print('idx: %d | CUSUM: %f | std: %f' % (j, cusumStats[-1][j], stdEsts[j]))
-        if cusumStats[-1][j] >= threshold:
-            # Backtrack to find where previous regime ended (where CUSUM stat was last small)
-            regLen = np.amax(np.where(batchOutput < threshold/4)) + 1
-            i += regLen
+        batchOut = cusum(x[i:j], x[i], stdEsts[j], threshold)
+        if len(batchOut) > 1 and np.any(batchOut >= threshold):
+            regimeLen = len(batchOut)
+            cusumStats[-1][i:i+regimeLen] = batchOut
+            i += regimeLen
             changePts.append(i)
-            
             cusumStats.append(np.full(n, np.nan))
             
             if verbose:
@@ -74,4 +79,82 @@ def cusum(x, startup = 10, threshold=6, max_influence=4, verbose=False):
         
         else:
             j += 1
-    return {'x': x, 'cusum': cusumStats, 'std': stdEsts}, changePts
+    cusumStats[-1][i:i+len(batchOut)] = batchOut
+    return changePts, pd.DataFrame(np.transpose(cusumStats)), stdEsts
+
+  
+
+# def batchCusum(x, mean, std, min_shift = .5, max_influence = 4):
+#     """CUSUM algorithm for change point detection
+#     x  : 1D np.array
+#     mean: estimated center of the distribution of x[0]
+#     std: estimated standard deviation of the distribution of x[0]
+#     min_shift: minimum shift size to consider
+#     max_influence: the maximum influence of a single data point
+#     """
+    
+#     # Normalize x
+#     x = (x - mean) / std
+    
+#     # Compute positive and negative CUSUM stats
+#     Cp = np.zeros_like(x)
+#     Cn = np.zeros_like(x)
+#     for i in range(len(x)):
+#         if i > 0:
+#             psignal = np.min([max_influence, x[i] - min_shift])
+#             nsignal = np.min([max_influence, -x[i] - min_shift])
+#             Cp[i] = np.max([0, psignal + Cp[i-1]])
+#             Cn[i] = np.max([0, nsignal + Cn[i-1]])
+            
+#     # Final statistic is max of Cp and Cn
+#     return np.maximum(Cp, Cn)
+
+# def cusum(x, startup = 10, threshold=6, max_influence=4, verbose=False):
+#     """Apply CUSUM algorithm after each new data point
+#     x: 1D np.array with at least (startup) points
+#     startup: number of points to bootstrap initial std guess
+#     threshold: threshold for cusum stat before declaring a change point
+#     verbose: controls debug messages
+#     """
+#     n = len(x)
+    
+#     # Things to keep track of
+#     cusumStats = [np.full(n, np.nan)]
+#     changePts = [0]
+#     stdEsts = np.zeros(n)
+    
+#     # Steal some initial data for an initial guess about std
+#     stdEsts[:startup] = np.std(x[:startup])
+#     preStd = stdEsts[startup-1]
+#     preWt = 5 # Use previous regime's stdev as if it was this much data
+    
+#     # Investigate the current regime for evidence of a new regime.
+#     i = changePts[-1]
+#     j = i+1
+#     while j < n:
+#         # Estimate variance of current regime
+#         stdEsts[j] = weightedStd(2*preStd, preWt, x[i:j])
+        
+#         # Calculate CUSUM stat and check if it's over the threshold
+#         batchOutput = batchCusum(x[i:j], np.median(x[i:j]), stdEsts[j], max_influence=max_influence)
+#         cusumStats[-1][j] = batchOutput[-1]
+#         if verbose: 
+#             print('idx: %d | CUSUM: %f | std: %f' % (j, cusumStats[-1][j], stdEsts[j]))
+#         if cusumStats[-1][j] >= threshold:
+#             # Backtrack to find where previous regime ended (where CUSUM stat was last small)
+#             regLen = np.amax(np.where(batchOutput < threshold/4)) + 1
+#             i += regLen
+#             changePts.append(i)
+            
+#             cusumStats.append(np.full(n, np.nan))
+            
+#             if verbose:
+#                 print('At idx %d, changepoint detected at idx %d' % (j, i))
+            
+#             # Now that we know i is a change point, go back to i and recalculate CUSUM
+#             preStd = stdEsts[i-1]
+#             j = i+1
+        
+#         else:
+#             j += 1
+#     return {'x': x, 'cusum': cusumStats, 'std': stdEsts}, changePts
--- utils.py
+++ utils.py
@@ -2,44 +2,41 @@ import datetime as dt
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Use median absolute deviation instead of std
-from scipy.stats import median_absolute_deviation as mad
+def trim(x, p, end):
+    # Trim most recent observations
+    if len(x) < 3:
+        return x
+    n = max(3, len(x)-end)
+    xt = x[:n]
+    # Trim most extreme observations if more than 2std away
+    n_remove = int(np.floor(n*p))
+    dev = np.abs(xt - xt.mean())/xt.std()
+    order = np.argsort(np.abs(xt - xt.mean()))
+    keep = [i for i in range(n) if dev[i] < 2 or i in order[:n-n_remove]] 
+    return xt[keep]
 
-def trim(x, p=.1):
-    n = len(x)
-    x_s = sorted(x)
-    start = int(np.floor(n*p))
-    end = n-start
-    return x[start:end]
-    
-    
-def weightedStd(priorStd, priorN, x):
-    """Weighted average of a prior estimate of std and std of current sample
-    """
-    n = len(x)
-    if n < 2:
-        return priorStd
-    
-    std = trim(x).std()
-    postStd = (priorN * priorStd + n * std) / (priorN + n)
-    return postStd
+def trimMean(x, p=.125, end=2):
+    return trim(x, p, end).mean()
 
-def plotRegimes(dates, values, changePts, ax):
-    n = len(values)
-    m = len(changePts)
-    for i in range(m):
-        a = changePts[i]
-        b = changePts[i+1] if i < m-1 else n
-        regimeAvg = trim(values[a:b]).mean() * np.ones(2)
-        regimeStd = trim(values[a:b]).std() * np.ones(2)
-        halfday = dt.timedelta(days=.5)
-        s = dates[a] - halfday
-        e = dates[b] - halfday if i < m-1 else dates[-1] + halfday
-        ax.axvline(x=s, color='red')
-        ax.plot([s, e], regimeAvg, color='red')
-        ax.plot([s, e], regimeAvg + 2*regimeStd, color='red', ls='--', lw=1)
-        ax.plot([s, e], regimeAvg - 2*regimeStd, color='red', ls='--', lw=1)
-        ax.plot(dates, values, color='blue')
+def trimStd(x, p=.125, end=2):
+    return trim(x, p, end).std()
+    
+# def plotRegimes(dates, values, changePts, ax):
+#     n = len(values)
+#     m = len(changePts)
+#     for i in range(m):
+#         a = changePts[i]
+#         b = changePts[i+1] if i < m-1 else n
+#         regimeAvg = trimMean(values[a:b]) * np.ones(2)
+#         regimeStd = trimMean(values[a:b]) * np.ones(2)
+#         halfday = dt.timedelta(days=.5)
+#         s = dates[a] - halfday
+#         e = dates[b] - halfday if i < m-1 else dates[-1] + halfday
+#         ax.axvline(x=s, color='red')
+#         ax.plot([s, e], regimeAvg, color='red')
+#         ax.plot([s, e], regimeAvg + 2*regimeStd, color='red', ls='--', lw=1)
+#         ax.plot([s, e], regimeAvg - 2*regimeStd, color='red', ls='--', lw=1)
+#         ax.plot(dates, values, color='blue')
         
 def regimeTimeseries(y, changePts):
     n = len(y)
@@ -49,10 +46,38 @@ def regimeTimeseries(y, changePts):
     for i in range(m):
         a = changePts[i]
         b = changePts[i+1] if i < m-1 else n
-        mean[a:b] = trim(y[a:b]).mean()
-        std[a:b] = trim(y[a:b]).std()
+        mean[a:b] = trimMean(y[a:b])
+        std[a:b] = trimStd(y[a:b])
     return mean, std
         
+def mergeChangePts(x):
+    '''Find consecutive chunks of changepoints. Decide which one is the
+    true changepoint by the mode, and break ties by taking the first.
+    '''
+    x = sorted(x)
+    chunks = []
+    tempChunk = [x[0]]
+    current = x[0]
+    i = 1
+    while i<len(x):
+        if x[i] == current or x[i] == current+1:
+            tempChunk.append(x[i])
+        else:
+            chunks.append(tempChunk)
+            tempChunk = [x[i]]
+            
+        current = x[i]
+        i += 1
+    chunks.append(tempChunk)
+    
+    output = []
+    for chunk in chunks:
+        truePt = chunk[0]
+        for y in chunk:
+            if chunk.count(y) > chunk.count(truePt):
+                truePt = y
+        output.append(truePt)
+    return output
         
         
         
