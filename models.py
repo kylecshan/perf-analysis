fd7918d18972984e1607eda4a24480b29be5ad32
Kyle Shan <kylecshan@gmail.com>
2020-02-22 20:07:58 -0800

    functions reorganized

--- models.py
+++ models.py
@@ -1,116 +1,122 @@
 import numpy as np
 import pandas as pd
 from scipy.stats import t
+from basicstats import *
 from utils import *
-    
-# def weightedStd(priorStd, priorN, x):
-#     """Weighted average of a prior estimate of std and std of current sample
-#     """
-#     n = len(x)
-#     if n < 4:
-#         return priorStd
-    
-#     std = trimStd(x)
-#     postStd = (priorN * priorStd + n * std) / (priorN + n)
-#     return postStd
 
-def trimmedStats(x, end=0):
-    return trimMean(x, end=0), trimStd(x, end=0)**2
-
-def ttest(xl, xr):
-    lmean, lvar = trimmedStats(xl)
-    rmean, rvar = trimmedStats(xr)
-
-    k = len(xl)
-    n = len(xl)+len(xr)
-    
-    rss = (k-1)*lvar + (n-k-1)*rvar
-    sigma = np.sqrt(rss/(n-2))
-    
-    T = np.sqrt(k*(n-k)/n)*(lmean-rmean)/sigma
-    return T
-    
-def glrmean(x, threshold, minRegime=3):
-    """Scan through range to find partition which maximizes t-test for change
-    in the mean
-    """
-    best = 0
+def glrmean(x, signif, minRegime=3):
+    '''
+    For each potential split point in a time series, perform a generalized
+    likelihood test for a difference in the mean (equivalent to t-test for normal)
+    Input:
+        x        : time series input
+        signif   : significance level (e.g. 0.001). Since we perform multiple
+                   hypothesis tests, we use the Bonferroni correction to avoid
+                   inflated p-values (e.g. if testing 8 potential changepoints,
+                   each changepoint is tested at the 0.001/8 significance level)
+        minRegime: minimum distance from the beginning to consider as a changepoint
+                   (e.g. if minRegime = 3, don't consider x[0], x[1], or x[2] as
+                   potential changepoints)
+    Output:
+        chgPt: None if no significant changepoint detected, otherwise the index of
+               the detected changepoint (where i is a changepoint iff there was a
+               change between i-1 and i)
+        stat : Highest t-stat detected (this is over the threshold iff chgPt is
+               not None). If not enough data, then 0.
+    '''
+    stat = 0
     chgPt = None
     n = len(x)
     if n <= minRegime:
         return chgPt, 0
     
     # Performing (n-minRegime) t-tests; apply Bonferroni correction
-    threshold = t.isf(threshold/(2*(n-minRegime)), df=n-2)
+    threshold = t.isf(signif/(2*(n-minRegime)), df=n-2)
     for k in range(minRegime, n):
         T = ttest(x[:k], x[k:])
-        if np.abs(T) > best:
-            best = np.abs(T)
+        if np.abs(T) > stat:
+            stat = np.abs(T)
             if np.abs(T) >= threshold: 
                 chgPt = k
-    return chgPt, best
+    return chgPt, stat
 
-def glrvar(x, threshold, minSample = 6):
-    """Scan through range to find partition which maximizes test for change
-    in the var
-    """
-    best = 0
-    chgPt = None
-    n = len(x)
-    if n < minSample:
-        return chgPt, 0
-    for k in range(minSample, n-minSample+1):
-        lmean, lvar = trimmedStats(xl)
-        rmean, rvar = trimmedStats(xr)
+# def glrvar(x, threshold, minSample = 6):
+#     """
+#         For each potential split point in a time series, perform a generalized
+#     likelihood test for a difference in the variance.
+#     Input:
+#         x        : time series input
+#         threshold: numeric threshold for detection (e.g. 10). No easy way to translate
+#                    between this value and a significance level.
+#         minRegime: minimum amount of data on either end of the regime
+#     Output:
+#         chgPt: None if no significant changepoint detected, otherwise the index of
+#                  the detected changepoint
+#         best : Highest statistic detected (this is over the threshold iff chgPt is
+#                not None). If not enough data, then 0.
+#     """
+#     best = 0
+#     chgPt = None
+#     n = len(x)
+#     if n < minSample:
+#         return chgPt, 0
+#     for k in range(minSample, n-minSample+1):
+#         lmean, lvar = trimmedStats(xl)
+#         rmean, rvar = trimmedStats(xr)
         
-        rss = (k-1)*lvar + (n-k-1)*rvar
-        var = rss/(n-2)
+#         rss = (k-1)*lvar + (n-k-1)*rvar
+#         var = rss/(n-2)
         
-        C = 1+( 1/(k-1) + 1/(n-k-1) - 1/(n-2) )/3
-        G = ( (k-1)*np.log(var/lvar) + (n-k-1)*np.log(var/rvar) ) / C
-        if G > best:
-            best = G
-            if G >= threshold:
-                chgPt = k
-    return chgPt, best
+#         C = 1+( 1/(k-1) + 1/(n-k-1) - 1/(n-2) )/3
+#         G = ( (k-1)*np.log(var/lvar) + (n-k-1)*np.log(var/rvar) ) / C
+#         if G > best:
+#             best = G
+#             if G >= threshold:
+#                 chgPt = k
+#     return chgPt, best
         
     
 def findChangePts(x, threshold=6, minRegime = 3, method='mean', verbose=False):
-    """Apply CUSUM algorithm after each new data point
-    x: 1D np.array
-    threshold: threshold for declaring a change point (interpretation depends on model)
-    minRegime: minimum number of data points between changepoints
-    method: whether to look for a change in mean ('mean') or variance ('var')
-    verbose: controls debug messages
-    """
+    '''
+    Apply changepoint detection method sequentially
+    Inputs:
+        x        : time series input
+        threshold: threshold value to pass into the underlying model
+        minRegime: minimum regime length (observations between changepoints) to allow
+        method   : test for 'mean' or 'var'
+        verbse   : print some output while using
+    Outputs:
+        changePts: list of detected changepoints (indices of x)
+        detectPts: list of indices at which corresponding changepoints were detected
+                   (a changepoint may not be detected until several observations after
+                   the change)
+    '''
     n = len(x)
-    if n < 4:
-        return [0]
+    if n < minRegime+1:
+        return [0], [0]
     assert(method in ['mean', 'var'])
     
-    # Investigate the current regime for evidence of a new regime.
     changePts = [0] # Identified changepoints
     detectPts = [0] # When each corresponding changepoint is detected
-    i = changePts[-1]
-    j = i+1
-    while j < n:
-        # Calculate stat and check if it's over the threshold
+    i = 0           # Track the last detected changepoint
+    j = i+1         # Consider data up to (but not including) index j
+    while j <= n:
+        # Find changepoints in x[i:j]
+        stat = 0
         if method == 'mean':
-            chgPt, _ = glrmean(x[i:j], threshold, minRegime)
-        elif method == 'var':
-            chgPt, _ = glrvar(x[i:j], threshold)
+            chgPt, stat = glrmean(x[i:j], threshold, minRegime)
+#         elif method == 'var':
+#             chgPt, _ = glrvar(x[i:j], threshold, minRegime)
         
         if chgPt is not None:
             i += chgPt
             changePts.append(i)
-            detectPts.append(j)
-            
+            detectPts.append(j-1)
             if verbose:
-                print('At idx %d, changepoint detected at idx %d' % (j, i))
+                print('At idx %d, changepoint detected at idx %d' % (j-1, i))
             
-            # Now that we know i is a change point, go back to i and recalculate CUSUM
+            # Now that we know i is a change point, go back to i
             j = i+1
-        
         else:
             j += 1
     return changePts, detectPts
--- utils.py
+++ utils.py
@@ -1,8 +1,17 @@
 import datetime as dt
 import matplotlib.pyplot as plt
 import numpy as np
+from IPython.display import HTML
 
 def trim(x, p=.125, end=0):
+    '''
+    Remove observations that may be outliers - suspiciously far from the average.
+    At most [floor(len(x)*p) + end] points are removed.
+    Inputs:
+        x  : data to trim
+        p  : proportion of data to remove
+        end: number of data points at the end of x to ignore
+    '''
     # Trim most recent observations
     if len(x) < 3:
         return x
@@ -15,58 +24,16 @@ def trim(x, p=.125, end=0):
     keep = [i for i in range(n) if dev[i] < 2 or i in order[:n-n_remove]] 
     return xt[keep]
 
-def trimMean(x, p=.125, end=0):
-    return trim(x, p, end).mean()
-
-def trimStd(x, p=.125, end=0):
-    return trim(x, p, end).std()
-    
-# def plotRegimes(dates, values, changePts, ax):
-#     n = len(values)
-#     m = len(changePts)
-#     for i in range(m):
-#         a = changePts[i]
-#         b = changePts[i+1] if i < m-1 else n
-#         regimeAvg = trimMean(values[a:b]) * np.ones(2)
-#         regimeStd = trimMean(values[a:b]) * np.ones(2)
-#         halfday = dt.timedelta(days=.5)
-#         s = dates[a] - halfday
-#         e = dates[b] - halfday if i < m-1 else dates[-1] + halfday
-#         ax.axvline(x=s, color='red')
-#         ax.plot([s, e], regimeAvg, color='red')
-#         ax.plot([s, e], regimeAvg + 2*regimeStd, color='red', ls='--', lw=1)
-#         ax.plot([s, e], regimeAvg - 2*regimeStd, color='red', ls='--', lw=1)
-#         ax.plot(dates, values, color='blue')
-        
-def regimeTimeseries(y, changePts):
-    n = len(y)
-    m = len(changePts)
-    mean = np.zeros_like(y)
-    std = np.zeros_like(y)
-    for i in range(m):
-        a = changePts[i]
-        b = changePts[i+1] if i < m-1 else n
-        mean[a:b] = trimMean(y[a:b])
-        std[a:b] = trimStd(y[a:b])
-    return mean, std
                
-# def regimeTimeseries2(x, y, changePts):
-#     n = len(y)
-#     m = len(changePts)
-#     newX = np.zeros(2*m)
-#     mean = np.zeros(2*m)
-#     std = np.zeros(2*m)
-#     for i in range(m):
-#         a = changePts[i]
-#         b = changePts[i+1] if i < m-1 else n
-#         newX[2*i], newX[2*i+1] = a, b-1
-#         mean[2*i] = mean[2*i+1] = trimMean(y[a:b])
-#         std[2*i] = std[2*i+1] = trimStd(y[a:b])
-#     return newX, mean, std
-
 def mergeChangePts(cps, voteThreshold=2):
     '''Given a list of list of changepoints, consolidate into a
-    master set of changepoints.
+    master set of changepoints. First, keep changepoints that multiple lists
+    agree on. Of the remaining, for each group of consecutive changepoints, pick
+    the earliest one. Output will have no consecutive changepoints.
+    Input:
+        cps          : list of lists of detected changepoints
+        voteThreshold: number of lists that must agree for a changepoint to be
+                         automatically voted in
     '''
     # Get max index voted for
     n = max([max(y) for y in cps]) + 1
@@ -99,8 +66,29 @@ def printEvents(events, mostRecent, recency):
                 c0 = c
     return {d: v for d, v in events.items() if mostRecent - d <= dt.timedelta(recency)}
         
-        
-        
+# https://stackoverflow.com/questions/27934885/how-to-hide-code-from-cells-in-ipython-notebook-visualized-with-nbviewer
+def hide_code_button():
+    '''
+    Create an HTML button that hides all code blocks in a jupyter notebook
+    '''
+    return HTML('''
+        <script>
+        code_show=true; 
+        function code_toggle() {
+         if (code_show){
+         $('div.input').hide();
+         } else {
+         $('div.input').show();
+         }
+         code_show = !code_show
+        } 
+        $( document ).ready(code_toggle);
+        </script>
+        <form action="javascript:code_toggle()">
+        <input type="submit" value="Show/hide code blocks">
+        </form>
+    ''')        
+
         
         
         
