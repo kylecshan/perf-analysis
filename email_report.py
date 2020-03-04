e35b2f6fe584643c7fc3f0925a08908a621254ea
Kyle Shan <kylecshan@gmail.com>
2020-03-04 11:30:09 -0800

    fixed minor inconsistency in parameters

--- email_report.py
+++ email_report.py
@@ -16,15 +16,20 @@ def changepoint_test(wtimes, alpha=0.005):
                but we are waiting for more observations to confirm
         'pass' otherwise (no performance drops detected)
     '''
-    wt = np.asarray(wtimes, dtype=np.float64)
-    # Find latest changepoint based on previously known information
-    chgpts, detpts, votes = find_chgpts(wt[:-1], alpha=alpha)
+    min_agree = 3 # Number of consecutive times a changepoint must be detected
+    num_test = 5  # Consider only the largest changes in timers as potential changepoints
+    lookback = 30 # Limit lookback window
     
-    # Only consider data since the latest previously-detected changepoint
-    last_known = chgpts[-1]
+    wt = np.asarray(wtimes, dtype=np.float64)
+    
+    # Find latest changepoint based on previously known information
+    chgpts, detpts, votes = find_chgpts(wt[:-1], alpha, min_agree, num_test, lookback)
+    
+    # Only consider data since the latest previously-known changepoint, subj to max lookback window
+    last_known = max(chgpts[-1], len(wt) - lookback)
     wt_recent = wt[last_known:]
     
-    vote_list, tstats = glrmean(wt_recent, alpha=alpha)
+    vote_list, tstats = glrmean(wt_recent, alpha, num_test)
     perf_drops = {vote+last_known: tstat for vote, tstat in zip(vote_list, tstats) if tstat > 0}
     
     votes.push(perf_drops)
